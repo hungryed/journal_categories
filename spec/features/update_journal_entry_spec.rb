b910443181902b4640504642a9273f8377c9a643
Joe Sutton <hungryed6@gmail.com>
2013-12-23 22:28:48 -0500

    Added more failing tests to work on for tomorrow

--- spec/features/update_journal_entry_spec.rb
+++ spec/features/update_journal_entry_spec.rb
@@ -1,7 +1,23 @@
 require 'spec_helper'
 
 feature 'user view existing post' do
-  scenario 'user can update current attributes of post' do
+
+  scenario 'user can click on existing journal entry' do
+    entry = FactoryGirl.create(:journal_entry)
+    visit journal_entries_path
+    click_on entry.title
+    click_on 'Submit'
+    expect(page).to have_content entry.title
+  end
+
+  scenario 'user can delete existing journal entry' do
+    entry = FactoryGirl.create(:journal_entry)
+    visit journal_entries_path
+    click_on 'Destroy'
+    expect(page).to_not have_content entry.title
+  end
+
+  scenario 'user can update current attributes of journal' do
     entry = FactoryGirl.create(:journal_entry)
     categories = FactoryGirl.create_list(:category, 3)
     visit journal_entry_path(entry)
@@ -10,8 +26,6 @@ feature 'user view existing post' do
     category1 = categories.first
     category2 = categories[1]
     category3 = categories.last
-    # fill_in 'Title', with: ''
-    # fill_in'Description', with: ''
     fill_in 'Title', with: 'Obscure Reference'
     fill_in'Description', with: 'Deep and meaningful content'
     expect(page).to have_content category1.name
@@ -19,7 +33,7 @@ feature 'user view existing post' do
     expect(page).to have_content category3.name
     click_on 'Submit'
     expect(page).to have_content 'Obscure Reference'
-    expect(page).to have_content entry.description
+    expect(page).to have_content 'Deep and meaningful content'
   end
 
 end
--- spec/features/user_creates_new_category_spec.rb
+++ spec/features/user_creates_new_category_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 feature 'User Creates New Category' do
+  after(:all) { Category.destory_all }
   scenario 'User creates new category listing' do
     visit categories_path
     click_on 'Add Category'
@@ -10,10 +11,27 @@ feature 'User Creates New Category' do
     expect(page).to have_content 'Lobster'
   end
 
+  scenario 'User cannot create multiple similar categories' do
+    visit new_category_path
+    fill_in 'Name', with: 'Kraken'
+    click_on 'Submit'
+    click_on 'Add Category'
+    fill_in 'Name', with: 'Kraken'
+    click_on 'Submit'
+    expect(page).to have_content 'That category already exists'
+  end
+
   scenario 'User sees current categories' do
     category = FactoryGirl.create(:category)
     visit categories_path
     expect(page).to have_content category.name
+  end
+
+  scenario 'User deletes existing category' do
+    category = FactoryGirl.create(:category)
+    visit categories_path
+    click_on 'Delete'
+    expect(page).to_not have_content category.name
   end
 
 end
